0d4d8ef3735255aaf879b699a7b8dd56426e97c2
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-13 20:13:44 +0300

    general things
    FPS ,  screen resolution setup and font.
    Also added main colours (white, black + RGB ones) for debugging in later

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import pygame
 import random    
 from pygame. constants import QUIT, K_s, K_w, K_a, K_d
 from os import listdir
+#general\\\\\\\\\\\\\\\\\\\\\\\\\\\
+pygame.init()
+
+FPS = pygame.time.Clock()
+
+screen = width, heigth = 1720, 1000
+
+font = pygame.font.SysFont('Verdana', 20)
+
+
+#colour_list\\\\\\\\\
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+RED = 255, 0, 0
+GREEN = 0, 0, 255
+BLUE = 0, 255, 0
+#colour_list/////////
