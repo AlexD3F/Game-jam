bed0391990a5808bd42bbd4469f4beb8d7a2e5c8
OleksandrSemenovych <146714857+AlexD3F@users.noreply.github.com>
2023-10-13 20:50:09 +0300

    now it can be launched + player var
    added background placeholder+now can be technicaly launched, but will crash on launch as not everything initialised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-import random    
-from pygame. constants import QUIT, K_s, K_w, K_a, K_d
+import random   
+from pygame.constants import QUIT, K_s, K_w, K_a, K_d
 from os import listdir
 
 #general\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -22,3 +22,29 @@ BLUE = 0, 255, 0
 #colour_list/////////
 
 main_surface = pygame.display.set_mode(screen)
+
+
+#ball\\\\\\\\\\\\\\\\\\\\\\\\\
+ball = pygame.Surface((20,20))
+ball.fill((WHITE))
+ball_rect = ball.get_rect()
+ball_speed = 7
+#ball/////////////////////////
+
+
+
+#background\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+bg = pygame.transform.scale(pygame.image.load('imgs/background.png').convert(), screen)
+bgx = 0
+bgx2 = bg.get_width()
+bg_speed = 3
+#background////////////////////////////////////////////////////////////////////////////////
+
+change_img = pygame.USEREVENT +3
+pygame.time.set_timer(change_img, 125)
+
+is_working = True
+
+while is_working:
+
+    FPS.tick(60)
